5749324e6b6efd716bf4e6167db3a12666a5e464
SwissCheese99 <64934810+SwissCheese99@users.noreply.github.com>
2020-05-07 11:13:47 +0200

    Quick

---
+++ Run/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  // put setup code here
+  createCanvas(400, 400);
+}
+
+function draw() {
+  // put drawing code here
+  background(180);
+  fill(50);
+  ellipse(height/2, width/2, 80, 80);
+}
